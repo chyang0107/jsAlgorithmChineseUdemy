c7998927d97445130910f7f3c066cd515e1955d7
chyang0107 <chyang0107@gmail.com>
2021-08-12 11:56:57 -0500

    practice

---
+++ app.js
@@ -0,0 +1,12 @@
+// //Array
+let arr = ["Harry", "Ron", "Snap"];
+// // JS for loop
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+//   console.log(i);
+// }
+
+// forEach
+arr.forEach((person) => {
+  console.log(person);
+});
