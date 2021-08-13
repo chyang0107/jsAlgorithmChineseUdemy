379c03c82d3c9b42305df827827a9c7ca002a567
chyang0107 <chyang0107@gmail.com>
2021-08-12 21:15:19 -0500

    practice

--- app.js
+++ app.js
@@ -7,28 +7,40 @@ let arr = ["Harry", "Ron", "Snap"];
 // }
 
 // forEach
-arr.forEach((person) => {
-  console.log(person);
-});
+// arr.forEach((person) => {
+//   console.log(person);
+// });
 
-// object
-let Harry = {
-  name: "Harry Porter",
-  age: 40,
-  married: true,
-  sayHi() {
-    console.log("Harry says hi to you");
-  },
-};
+// // object
+// let Harry = {
+//   name: "Harry Porter",
+//   age: 40,
+//   married: true,
+//   sayHi() {
+//     console.log("Harry says hi to you");
+//   },
+// };
 
-console.log(Harry.age);
-console.log(Harry["age"]);
-Harry.sayHi();
-// function
-function add(n1, n2) {
-  return n1 + n2;
-  // The codes under the 'return' could not executible
-  console.log(n1 + n2);
+// console.log(Harry.age);
+// console.log(Harry["age"]);
+// Harry.sayHi();
+// // function
+// function add(n1, n2) {
+//   return n1 + n2;
+//   // The codes under the 'return' could not executible
+//   console.log(n1 + n2);
+// }
+
+// console.log(add(10, 15));
+// Class
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;  
+  }
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
 }
 
-console.log(add(10, 15));
+let c1 = new Circle(5);
